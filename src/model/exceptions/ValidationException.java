46bbe75f5436202b407311fb4a46e5bd86353a2b
Randolpho Alencar <randolphoalencar@gmail.com>
2022-09-30 21:22:44 -0300

    Update department

--- src/model/exceptions/ValidationException.java
+++ src/model/exceptions/ValidationException.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ValidationException extends RuntimeException {
-
 	private static final long serialVersionUID = 1L;
 	
 	private Map<String, String> errors = new HashMap<>(); 
